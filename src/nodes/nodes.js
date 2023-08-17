a5c89b64959e1c1042e95b42cf95edd734a7a11c
Andrew Punnett <andrew.punnett@gmail.com>
2023-08-17 17:53:38 +1200

    Don't display the example nodes in the GUI.

--- src/nodes/nodes.js
+++ src/nodes/nodes.js
@@ -1,7 +1,7 @@
-import { default as ExampleSimple } from "./ExampleSimple/ExampleSimple.Component";
-import { default as ExampleComposite } from "./ExampleComposite/ExampleComposite.Component";
-import { default as ExampleAudioWorklet } from "./ExampleAudioWorklet/ExampleAudioWorklet.Component";
-import exampleWorkletProcessorUrl from "./ExampleAudioWorklet/ExampleAudioWorklet.Processor.js?url";
+// import { default as ExampleSimple } from "./ExampleSimple/ExampleSimple.Component";
+// import { default as ExampleComposite } from "./ExampleComposite/ExampleComposite.Component";
+// import { default as ExampleAudioWorklet } from "./ExampleAudioWorklet/ExampleAudioWorklet.Component";
+// import exampleWorkletProcessorUrl from "./ExampleAudioWorklet/ExampleAudioWorklet.Processor.js?url";
 import { default as AudioOutput } from "./AudioOutput/AudioOutput.Component";
 import { default as Adsr } from "./Adsr/Adsr.Component";
 import adsrProcessorUrl from "./Adsr/Adsr.Processor.js?url";
@@ -11,9 +11,9 @@ import { default as Vco } from "./Vco/Vco.Component";
 import { default as Lfo } from "./Vco/Lfo.Component";
 
 export const availableNodes = new Map([
-  [ExampleSimple.key, ExampleSimple],
-  [ExampleAudioWorklet.key, ExampleAudioWorklet],
-  [ExampleComposite.key, ExampleComposite],
+  //  [ExampleSimple.key, ExampleSimple],
+  //  [ExampleAudioWorklet.key, ExampleAudioWorklet],
+  //  [ExampleComposite.key, ExampleComposite],
   [AudioOutput.key, AudioOutput],
   [Adsr.key, Adsr],
   [GateButton.key, GateButton],
@@ -24,7 +24,7 @@ export const availableNodes = new Map([
 
 export async function addModules(context) {
   await Promise.all([
-    context.audioWorklet.addModule(exampleWorkletProcessorUrl),
+    //    context.audioWorklet.addModule(exampleWorkletProcessorUrl),
     context.audioWorklet.addModule(adsrProcessorUrl),
   ]).catch((reason) => console.error(reason));
 }
